fc527ed192fa9be59d4a5e610d4430297e259e12
Bryan Stopp <bryan.stopp@gmail.com>
2024-05-15 10:17:36 -0700

    Add base config for Tools

---
+++ tools/sidekick/admin/admin.js
@@ -0,0 +1,13 @@
+export async function decorate(a, b, c, d, e, f) {
+  // console.log(`A ${JSON.stringify(a)}`);
+  console.log(`B ${JSON.stringify(b)}`);
+  console.log(`C ${c}`);
+  console.log(`D ${JSON.stringify(d)}`);
+  console.log(`E ${JSON.stringify(e)}`);
+  console.log(`F ${JSON.stringify(f)}`);
+}
+
+export default {
+  title: 'Admin',
+  searchEnabled: true,
+};
